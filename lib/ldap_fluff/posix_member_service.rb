4d46cd24096ab549c5ca503668d5f31ff12dc253
amalbekh <bakhtaouiamal@gmail.com>
2017-06-21 15:00:00 +0200

    Update posix_member_service.rb

--- lib/ldap_fluff/posix_member_service.rb
+++ lib/ldap_fluff/posix_member_service.rb
@@ -20,7 +20,7 @@ class LdapFluff::Posix::MemberService < LdapFluff::GenericMemberService
     groups = []
     @ldap.search(:filter => Net::LDAP::Filter.eq('memberuid', uid),
                  :base => @group_base).each do |entry|
-      groups << entry[:distinguishedName][0]
+      groups << entry[:dn][0]
     end
     groups
   end
