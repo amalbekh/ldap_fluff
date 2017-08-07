45177f06dfa6aa71d24f2a0dc046dc8023f60996
amalbekh <bakhtaouiamal@gmail.com>
2017-08-07 11:14:33 +0200

    Update ldap_fluff.rb

--- lib/ldap_fluff/ldap_fluff.rb
+++ lib/ldap_fluff/ldap_fluff.rb
@@ -66,6 +66,12 @@ class LdapFluff
       @ldap.name_direction_for_uid(uid)
     end
   end
+  
+   def name_dga(uid)
+    instrument('name_dga.ldap_fluff', :uid => uid) do |payload|
+      @ldap.name_dga_for_uid(uid)
+    end
+  end
 
   # return true if a user is in all of the groups
   # in grouplist
