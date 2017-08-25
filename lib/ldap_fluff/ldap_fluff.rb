e890a768a42a586929539776db367516425515ae
amalbekh <bakhtaouiamal@gmail.com>
2017-08-25 15:31:43 +0200

    Update ldap_fluff.rb

--- lib/ldap_fluff/ldap_fluff.rb
+++ lib/ldap_fluff/ldap_fluff.rb
@@ -102,6 +102,12 @@ class LdapFluff
       @ldap.division_for_uid(uid)
     end
   end
+  
+  def firstName(uid)
+    instrument('firstName.ldap_fluff', :uid => uid) do |payload|
+      @ldap.firstName_for_uid(uid)
+    end
+  end
 
   # return true if a user is in all of the groups
   # in grouplist
