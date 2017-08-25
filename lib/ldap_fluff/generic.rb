f5d53d6160bae10358913dd1992233e13cbdb190
amalbekh <bakhtaouiamal@gmail.com>
2017-08-25 16:02:09 +0200

    Update generic.rb

--- lib/ldap_fluff/generic.rb
+++ lib/ldap_fluff/generic.rb
@@ -102,6 +102,13 @@ class LdapFluff::Generic
     return []
   end
   
+  def lastName_for_uid(uid)
+    service_bind
+    @member_service.find_user_lastName(uid)
+  rescue self.class::MemberService::UIDNotFoundException
+    return []
+  end
+  
   def division_for_uid(uid)
     service_bind
     @member_service.find_user_division(uid)
